dd6c67df45e3063ba1ae9a2bf4121068d4f17b51
ElvinYusifli <ElvinYusifli@users.noreply.github.com>
2015-11-07 23:09:17 +0400

    Update cbar.php

--- cbar.php
+++ cbar.php
@@ -1,17 +1,17 @@
 <?php 
-$url = 'http://www.cbar.az/currencies/'.date('d.m.Y') . '.xml';
-$doc = new \DOMDocument();
-$doc->load($url); 
-$items = $doc->getElementsByTagName('Valute'); 
-for ($i = 0; $i < $items->length; $i++) {
-$el = $items->item($i);
-switch ($el->getAttribute('Code')) {
-case 'USD':
-echo 'USD-'.$el->getElementsByTagName("Value")->item(0)->nodeValue;
-break;
-case 'RUB':
-echo 'RUB-'.$el->getElementsByTagName("Value")->item(0)->nodeValue;
-break;
-}
-}
+  $url = 'http://www.cbar.az/currencies/'.date('d.m.Y') . '.xml';
+  $doc = new \DOMDocument();
+  $doc->load($url); 
+  $items = $doc->getElementsByTagName('Valute'); 
+    for ($i = 0; $i < $items->length; $i++) {
+    $el = $items->item($i);
+    switch ($el->getAttribute('Code')) {
+        case 'USD':
+        echo 'USD-'.$el->getElementsByTagName("Value")->item(0)->nodeValue;
+        break;
+        case 'RUB':
+        echo 'RUB-'.$el->getElementsByTagName("Value")->item(0)->nodeValue;
+        break;
+    }
+  }
 ?>
